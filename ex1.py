9fe261cc096586dbc2e55bbbf6039cce1803384c
MarcusTrabalho <pizzimentimarcus@usp.br>
2024-08-27 18:58:51 -0300

    commit ex1 python

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+nome = sys.argv[1]
+cor = sys.argv[2]
+atividade = sys.argv[3]
+animal = sys.argv[4]
+
+print("Meu nome:" +  nome, ", Minha cor favorita:" +  cor, ", Minha atividade predileta:" +  atividade, ", Meu animal preferido:"+  animal)
